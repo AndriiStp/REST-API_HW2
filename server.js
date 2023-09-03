b96398c8784a1dab88a38d70fd71609f82698bb7
Andrii Stepanenko <andrii.stp@gmail.com>
2023-09-03 15:15:04 +0300

    init mongoose

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://AndriiStp:wgT4BuTdxYLs1Gsy@cluster0.6lf9fue.mongodb.net/db-contacts";
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => app.listen(3000))
+  .catch((error) => console.log(error.message));
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
