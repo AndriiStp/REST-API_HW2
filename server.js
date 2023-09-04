2da370b79f7b59320a9a4f6e0a75871f7a4dead6
Andrii Stepanenko <andrii.stp@gmail.com>
2023-09-04 21:22:48 +0300

    new key2

--- server.js
+++ server.js
@@ -8,11 +8,9 @@ mongoose
   .connect(DB_HOST)
   .then(() => app.listen(PORT))
   .catch((error) => {
-    console.log(error.message);
-    process.exit(1);
+    console.log(error);
   });
 
-console.log(DB_HOST);
 // app.listen(3000, () => {
 //   console.log("Server running. Use our API on port: 3000");
 // });
