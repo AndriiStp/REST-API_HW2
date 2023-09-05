a6e92b2028bee986748a472ec44f30c0f7ee3350
Andrii Stepanenko <andrii.stp@gmail.com>
2023-09-05 15:44:25 +0300

    fix mvalidateBody export

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -5,7 +5,7 @@ const { HttpError } = require("../helpers");
 const isValidId = (req, res, next) => {
   const { id } = req.params;
   if (!isValidObjectId(id)) {
-    next(HttpError(400, `${id} is not a valid`));
+    next(HttpError(400, `${id} is not a valid id`));
   }
   next();
 };
