234cf65cf738aa17d3e3a118f47775af7ffd2e86
Andrii Stepanenko <andrii.stp@gmail.com>
2023-09-05 15:31:20 +0300

    full with mistake

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,49 +1,58 @@
-const Contact = require("../models/contact");
+const { Contact } = require("../models/contact");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res, next) => {
   const result = await Contact.find();
-  res.json(result);
+  res.status(200).json(result);
 };
 
-// const getById = async (req, res, next) => {
-//   const { id } = req.params;
-//   const result = await contacts.getContactById(id);
-//   if (!result) {
-//     throw HttpError(404, "Not Found");
-//   }
-//   res.status(200).json(result);
-// };
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json(result);
+};
 
-// const addNew = async (req, res, next) => {
-//   const { name, email, phone } = req.body;
-//   const result = await contacts.addContact(name, email, phone);
-//   res.status(201).json(result);
-// };
+const addNew = async (req, res, next) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
 
-// const updateById = async (req, res, next) => {
-//   const { id } = req.params;
-//   const result = await contacts.updateContact(id, req.body);
-//   if (!result) {
-//     throw HttpError(404, "Not Found");
-//   }
-//   res.status(200).json(result);
-// };
+const updateById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json(result);
+};
 
-// const deleteById = async (req, res, next) => {
-//   const { id } = req.params;
-//   const result = await contacts.removeContact(id);
-//   if (!result) {
-//     throw HttpError(404, "Not Found");
-//   }
-//   res.status(200).json({ message: "Deleted" });
-// };
+const updateFavorite = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json(result);
+};
+
+const deleteById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
+  if (!result) {
+    throw HttpError(404, "Not Found");
+  }
+  res.status(200).json({ message: "Deleted" });
+};
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  // getById: ctrlWrapper(getById),
-  // addNew: ctrlWrapper(addNew),
-  // updateById: ctrlWrapper(updateById),
-  // deleteById: ctrlWrapper(deleteById),
+  getById: ctrlWrapper(getById),
+  addNew: ctrlWrapper(addNew),
+  updateById: ctrlWrapper(updateById),
+  updateFavorite: ctrlWrapper(updateFavorite),
+  deleteById: ctrlWrapper(deleteById),
 };
